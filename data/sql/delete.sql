9a357d28ae166680dab5a0884ff2f398b839be4b
Aina Ny Antsa Ratovonandrasana <nyantsa.ratovo@gmail.com>
2025-06-06 22:06:19 +0300

    feat:Setting up the mcd looping of the doctypes used for the project

--- data/sql/delete.sql
+++ data/sql/delete.sql
@@ -1,7 +1,7 @@
 delete from tabCompany where name='My Company';
 delete from tabEmployee;
-delete from `tabSalary Structure` ;
+delete from `tabSalary Structure`;
 delete from `tabSalary Component` ;
 DELETE from `tabSalary Structure Assignment` ;
-DELETE  from `tabSalary Detail`  ;
-delete from `tabSalary Slip` ;
+DELETE  from `tabSalary Detail`;
+delete from `tabSalary Slip`;
